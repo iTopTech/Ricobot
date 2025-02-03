7248769efd4132610538a43b2fdec1a0348dffd4
iTopTech <0xcharlesmlee@gmail.com>
2025-02-02 19:39:50 -0600

    apply mobile responsive

--- src/App.tsx
+++ src/App.tsx
@@ -13,68 +13,79 @@ const imgList = [
 function App() {
   const [currentIndex, setCurrentIndex] = useState(0);
   return (
-    <div className="min-h-screen w-screen overflow-x-hidden bg-white">
-      <div className="relative w-screen h-screen my-[300px]">
-        <img
-          src="/images/bg-1.png"
-          alt="Background"
-          className="w-full h-full object-cover"
-        />
-        <img
-          src="/images/cutout-1.png"
-          alt="cutout"
-          className="absolute inset-x-1/2 z-10 bottom-0"
-        />
-        <div className="gradient-lr"></div>
-        <div className="gradient-tb"></div>
-        <div className="absolute inset-0 flex flex-col justify-start items-start p-8 md:p-24">
-          <h2 className="text-[17px] md:text-[31px] font-black text-white mb-2 uppercase leading-none">
-            more from
-            <br />
-            rico the dog
-          </h2>
-          <span className="text-xs mt-20 md:text-sm font-extrabold text-white mb-4 bg-transparent border-[2px] border-white rounded-3xl py-2 px-3">
-            RICO IS BACK!
-          </span>
-          <h1 className="text-[49px] md:text-[35px] font-black text-white mb-4">
-            RICOBOT
-          </h1>
-          <p className="text-sm md:text-base font-medium w-1/2 text-white mb-6 max-w-md">
-            Charge into a brand-new supersized adventure with RICO across 50
-            exciting and diverse worlds, available now on PS5!
-          </p>
-          <button className="bg-white mt-8 text-[#09101A] text-sm md:text-base font-black px-6 py-3 rounded-3xl uppercase">
-            Learn more
-          </button>
-          <div className="flex flex-row gap-16 mt-16 items-end z-20">
+    <div className="min-h-screen w-screen overflow-x-hidden bg-white mb-[50px] md:mb-0">
+      <div className="relative w-screen h-fit md:h-screen my-[50px] md:my-[250px] overflow-visible">
+        <div
+          className="relative md:inline z-10 h-screen overflow-visible bg-no-repeat"
+          style={{
+            backgroundImage: 'url("/images/bg-1.png")',
+            backgroundSize: "100% auto",
+          }}
+        >
+          <img
+            src="/images/bg-1.png"
+            alt="Background"
+            className="w-fit md:w-full md:h-full z-20 object-cover"
+          />
+          <img
+            src="/images/cutout-1.png"
+            alt="cutout"
+            className="absolute inset-x-1/2 z-20 w-[40vw] md:w-[28vw] -top-8 md:-top-36"
+          />
+          <div className="gradient-lr z-20"></div>
+          <div className="gradient-tb z-20"></div>
+        </div>
+        <div className="absolute top-0 flex flex-col justify-start items-start py-6 px-8 md:p-24 bg-black md:bg-transparent h-fit mb-[50px] md:mb-0">
+          <div className="flex flex-col z-20">
+            <h2 className="text-[17px] md:text-[31px] font-black text-white mb-2 uppercase leading-none">
+              more from
+              <br />
+              rico the dog
+            </h2>
+            <span className="text-xs w-fit mt-24 md:mt-20 md:text-sm font-extrabold text-white mb-1 md:mb-4 bg-transparent border-[2px] border-white rounded-3xl py-2 px-3">
+              RICO IS BACK!
+            </span>
+            <h1 className="text-[49px] md:text-[35px] font-black text-white mb-4">
+              RICOBOT
+            </h1>
+            <p className="text-sm md:text-base font-medium md:w-1/2 text-white mb-6">
+              Charge into a brand-new supersized adventure with RICO across 50
+              exciting and diverse worlds, available now on PS5!
+            </p>
+            <button className="bg-white mt-4 md:mt-8 w-fit text-[#09101A] text-sm md:text-base font-black px-6 py-3 rounded-3xl uppercase">
+              Learn more
+            </button>
+          </div>
+          <div className="grid grid-cols-3 gap-1 min-h-fit w-full md:w-auto md:flex md:flex-row md:gap-16 mt-12 md:mt-16 justify-center md:justify-normal items-center md:items-end z-20">
             {imgList.map((img, index) => {
               return currentIndex === index ? (
-                <div className="bg-transparent gradient-border transition-all cursor-pointer">
+                <div
+                  key={index}
+                  className="bg-transparent min-w-fit max-w-fit gradient-border cursor-pointer justify-self-center"
+                >
                   <img
                     src={img}
-                    className="rounded-lg w-48 h-48"
+                    className="rounded-md md:rounded-lg w-24 h-24 md:w-48 md:h-48"
                     alt={`thumbnail${index}`}
                   />
                 </div>
               ) : (
-                <img
-                  src={img}
-                  className="rounded-lg w-32 h-32 cursor-pointer transition-all"
-                  alt={`thumbnail${index}`}
-                  onClick={() => setCurrentIndex(index)}
-                />
+                <div
+                  key={index}
+                  className="flex w-fit min-h-[106px] md:h-auto justify-center items-center justify-self-center"
+                >
+                  <img
+                    src={img}
+                    className="rounded-lg w-24 h-24 md:w-32 md:h-32 cursor-pointer"
+                    alt={`thumbnail${index}`}
+                    onClick={() => setCurrentIndex(index)}
+                  />
+                </div>
               );
             })}
           </div>
         </div>
       </div>
-      {/* <div className="relative">
-        <img
-          src="/images/foreground-dog.png"
-          alt="Foreground Dog"
-          className="absolute bottom-0 right-0 w-2/5 md:w-1/2 max-h-[calc(100%+70px)] md:max-h-[calc(100%+90px)]"
-        />
-      </div> */}
     </div>
   );
 }
